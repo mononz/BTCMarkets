17630ff18950ed3fc8439bee0e6ac806e17eefc5
Jared Hall <jared@mononz.com>
2018-08-27 00:26:04 +1000

    Adding sample implementation of the tick query

--- app/src/main/java/com/mononz/btcmarketssample/MainActivity.java
+++ app/src/main/java/com/mononz/btcmarketssample/MainActivity.java
@@ -1,13 +1,24 @@
 package com.mononz.btcmarketssample;
 
 import androidx.appcompat.app.AppCompatActivity;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.CompositeDisposable;
+import io.reactivex.observers.DisposableObserver;
+import io.reactivex.schedulers.Schedulers;
 
 import android.os.Bundle;
 import android.widget.TextView;
 
+import com.mononz.btcmarkets.BTCMarketsClient;
+import com.mononz.btcmarkets.MarketsTick;
+
 public class MainActivity extends AppCompatActivity {
 
     private TextView text;
+
+    private BTCMarketsClient client;
+
+    private CompositeDisposable disposables = new CompositeDisposable();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +27,26 @@ public class MainActivity extends AppCompatActivity {
 
         text = findViewById(R.id.text);
 
-        text.setText("Hello World!");
+        client = new BTCMarketsClient(this);
+        client.setDebug(true);
+
+        disposables.add(client.getMarketTick("XRP", "AUD")
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribeWith(new DisposableObserver<MarketsTick>() {
+                    @Override
+                    public void onNext(MarketsTick tick) {
+                        text.setText(tick.toString());
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+                        e.printStackTrace();
+                        text.setText(e.getMessage());
+                    }
+
+                    @Override
+                    public void onComplete() { }
+                }));
     }
 }
